5b4c00ad9c4cc12343af181358538924f8d9d79d
Fatih Akturk <f.akturk@gmail.com>
2017-04-20 11:00:21 +0900

    asd

--- app/src/main/java/netlab/fakturk/anglecalculator/MainActivity.java
+++ app/src/main/java/netlab/fakturk/anglecalculator/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static android.hardware.SensorManager.GRAVITY_EARTH;
@@ -21,16 +23,24 @@ public class MainActivity extends AppCompatActivity
     }
 
     int degree=0;
-    float[] accSDK, accNDK,gravity;
-    float angle = 0, angleCorrected=0;
+    float[] accSDK, accNDK,gravity, calibration, accCor, without;
+    float[] velSDK, velNDK, velCor, velWithout;
+    float[] disSDK, disNDK, disCor, disWithout;
+
+    float angle = 0, angleCorrected=0, currentAngle=0;
     AccSensorErrorData errorData;
     Gravity g;
     Orientation orientation;
-    TextView angle_tv, angleCorrected_tv, difference_tv;
+    TextView angle_tv, angleCorrected_tv, difference_tv, calibrate_tv, accNDK_tv,accSDK_tv, accCor_tv,sinCos_tv,without_tv;
+    TextView velNDK_tv,velSDK_tv, velCor_tv,velWithout_tv;
+    TextView disNDK_tv,disSDK_tv, disCor_tv,disWithout_tv;
+
     Newton newton;
+    Button buttonCalibrate;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected void onCreate(final Bundle savedInstanceState)
     {
 
         super.onCreate(savedInstanceState);
@@ -39,13 +49,46 @@ public class MainActivity extends AppCompatActivity
         startService(new Intent(this, SensorService.class));
         accSDK = new float[]{0,0,0};
         accNDK = new float[]{0,0,0};
+        accCor = new float[]{0,0,0};
+        without = new float[]{0,0,0};
+
+        velSDK = new float[]{0,0,0};
+        velNDK = new float[]{0,0,0};
+        velCor = new float[]{0,0,0};
+        velWithout = new float[]{0,0,0};
+
+        disSDK = new float[]{0,0,0};
+        disNDK = new float[]{0,0,0};
+        disCor = new float[]{0,0,0};
+        disWithout = new float[]{0,0,0};
+        
+        calibration = new float[]{0,0,0};
         gravity = new float[3];
         g = new Gravity();
         orientation = new Orientation();
         newton = new Newton();
+        errorData = new AccSensorErrorData();
         angle_tv = (TextView) findViewById(R.id.angle_tv_value);
         angleCorrected_tv = (TextView) findViewById(R.id.angleCorrected_tv_value);
         difference_tv = (TextView) findViewById(R.id.difference_tv_value);
+        calibrate_tv = (TextView) findViewById(R.id.calibrate_tv);
+        accNDK_tv = (TextView) findViewById(R.id.accNDK_tv_value);
+        accSDK_tv = (TextView) findViewById(R.id.accSDK_tv_value);
+        accCor_tv = (TextView) findViewById(R.id.accCorr_tv_value);
+        sinCos_tv = (TextView) findViewById(R.id.sinCos_tv);
+        without_tv = (TextView) findViewById(R.id.without_tv_value);
+        
+        velNDK_tv = (TextView) findViewById(R.id.velNDK_tv_value);
+        velSDK_tv = (TextView) findViewById(R.id.velSDK_tv_value);
+        velCor_tv = (TextView) findViewById(R.id.velCorr_tv_value);
+        velWithout_tv = (TextView) findViewById(R.id.velWithout_tv_value);
+
+        disNDK_tv = (TextView) findViewById(R.id.disNDK_tv_value);
+        disSDK_tv = (TextView) findViewById(R.id.disSDK_tv_value);
+        disCor_tv = (TextView) findViewById(R.id.disCorr_tv_value);
+        disWithout_tv = (TextView) findViewById(R.id.disWithout_tv_value);
+        
+        buttonCalibrate = (Button) findViewById(R.id.buttonCalibrate);
 
         LocalBroadcastManager.getInstance(this).registerReceiver(new BroadcastReceiver()
         {
@@ -57,6 +100,25 @@ public class MainActivity extends AppCompatActivity
 
             }
         }, new IntentFilter(SensorService.ACTION_SENSOR_BROADCAST));
+
+        buttonCalibrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (buttonCalibrate==v)
+                {
+                    float[] erroZero = errorData.getError("htc",0);
+                    for (int i = 0; i < 3; i++) {
+                        calibration[i]=gravity[i];
+                    }
+                    gravity[0]=0;
+                    gravity[1]=0;
+                    gravity[2]=GRAVITY_EARTH;
+                    calibration[2]=calibration[2]-GRAVITY_EARTH;
+                    calibrate_tv.setText(calibration[0]+", "+calibration[1]+", "+calibration[2]) ;
+                    newton.setCalibration(calibration);
+                }
+            }
+        });
     }
 
     /**
@@ -73,18 +135,52 @@ public class MainActivity extends AppCompatActivity
         calculateAngle(accNDK);
         float accNorm = (float) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
 
-        gravity[0] = x * (GRAVITY_EARTH / accNorm);
-        gravity[1] = y * (GRAVITY_EARTH / accNorm);
-        gravity[2] = z * (GRAVITY_EARTH / accNorm);
+        gravity[0] = x * (GRAVITY_EARTH / accNorm)-calibration[0];
+        gravity[1] = y * (GRAVITY_EARTH / accNorm)-calibration[1];
+        gravity[2] = z * (GRAVITY_EARTH / accNorm)-calibration[2];
         angle = (float) Math.toDegrees( g.angleBetweenGravity(gravity)) ;
         if(angle<0)
         {
             angle=360+angle;
         }
         angle_tv.setText(Float.toString(angle));
-        angleCorrected = newton.iterate(angle,accNDK);
+        sinCos_tv.setText(String.format("%.02f", Math.sin(Math.toRadians(angle))*GRAVITY_EARTH)+", "+String.format("%.02f", Math.cos(Math.toRadians(angle))*GRAVITY_EARTH));
+        accCor=newton.getAcc(accNDK,angle);
+        without=newton.getAccWithoutError(accNDK,angle);
+        if (Math.abs(angle-currentAngle)>1) {
+            angleCorrected = newton.iterate(angle, accNDK);
+        }
         angleCorrected_tv.setText(Float.toString(angleCorrected));
         difference_tv.setText(Float.toString(angle-angleCorrected));
+
+        accNDK_tv.setText(String.format("%.02f", x)+", "+String.format("%.02f", y)+", "+String.format("%.02f", z));
+        accSDK_tv.setText(String.format("%.02f", accSDK[0])+", "+String.format("%.02f", accSDK[1])+", "+String.format("%.02f", accSDK[2]));
+        accCor_tv.setText(String.format("%.02f", accCor[0])+", "+String.format("%.02f", accCor[1])+", "+String.format("%.02f", accCor[2]));
+        without_tv.setText(String.format("%.02f", without[0])+", "+String.format("%.02f", without[1])+", "+String.format("%.02f", without[2]));
+
+        for (int i = 0; i < 3; i++) {
+            velNDK[i]+=accNDK[i]*0.01;
+            velSDK[i]+=accSDK[i]*0.01;
+            velCor[i]+=accCor[i]*0.01;
+            velWithout[i]+=without[i]*0.01;
+        }
+        for (int i = 0; i < 3; i++) {
+            disNDK[i]+=velNDK[i]*0.01;
+            disSDK[i]+=velSDK[i]*0.01;
+            disCor[i]+=velCor[i]*0.01;
+            disWithout[i]+=velWithout[i]*0.01;
+        }
+
+        velNDK_tv.setText(String.format("%.02f", velNDK[0])+", "+String.format("%.02f", velNDK[1])+", "+String.format("%.02f", velNDK[2]));
+        velSDK_tv.setText(String.format("%.02f", velSDK[0])+", "+String.format("%.02f", velSDK[1])+", "+String.format("%.02f", velSDK[2]));
+        velCor_tv.setText(String.format("%.02f", velCor[0])+", "+String.format("%.02f", velCor[1])+", "+String.format("%.02f", velCor[2]));
+        velWithout_tv.setText(String.format("%.02f", velWithout[0])+", "+String.format("%.02f", velWithout[1])+", "+String.format("%.02f", velWithout[2]));
+
+        disNDK_tv.setText(String.format("%.02f", disNDK[0])+", "+String.format("%.02f", disNDK[1])+", "+String.format("%.02f", disNDK[2]));
+        disSDK_tv.setText(String.format("%.02f", disSDK[0])+", "+String.format("%.02f", disSDK[1])+", "+String.format("%.02f", disSDK[2]));
+        disCor_tv.setText(String.format("%.02f", disCor[0])+", "+String.format("%.02f", disCor[1])+", "+String.format("%.02f", disCor[2]));
+        disWithout_tv.setText(String.format("%.02f", disWithout[0])+", "+String.format("%.02f", disWithout[1])+", "+String.format("%.02f", disWithout[2]));
+
 
         String accStr =   x + " " + y + " " + z+ " " + accSDK[0] + " " + accSDK[1] + " " + accSDK[2]+"\n";
 
--- app/src/main/java/netlab/fakturk/anglecalculator/Newton.java
+++ app/src/main/java/netlab/fakturk/anglecalculator/Newton.java
@@ -10,30 +10,47 @@ public class Newton
 {
     float epsilon;
     AccSensorErrorData errorData;
+    float[] calibration;
 
 
     public Newton(float epsilon)
     {
 
         this.epsilon = epsilon;
-        errorData = new AccSensorErrorData();
+        init();
 
     }
     public Newton()
     {
 
-        this.epsilon = 0.01f;
-        errorData = new AccSensorErrorData();
+        this.epsilon = 0.1f;
+        init();
 
+
+
+    }
+    void init()
+    {
+        errorData = new AccSensorErrorData();
+        calibration=new float[]{0,0,0};
+    }
+    void setCalibration(float[] cal)
+    {
+        for (int i = 0; i < 3; i++) {
+//            calibration[i]=cal[i];
+        }
     }
 
     float firstDerivative(float[] acc, float alpha)
     {
         float fd=0;
+        for (int i = 0; i < 3; i++) {
+            acc[i]=acc[i]+calibration[i];
+        }
         float[] slopeOfError = errorData.getSlope("htc", alpha);
         float[] error = errorData.getError("htc", alpha);
-        float cosTerm= (float) (Math.cos(alpha)*GRAVITY_EARTH);
-        float sinTerm= (float) (Math.sin(alpha)*GRAVITY_EARTH);
+        float cosTerm= (float) (Math.cos(Math.toRadians(alpha))*GRAVITY_EARTH);
+        float sinTerm= (float) (Math.sin(Math.toRadians(alpha))*GRAVITY_EARTH);
 
         fd  =    2*(acc[0]-error[0])*slopeOfError[0]
                 +2*(acc[1]+sinTerm-error[1])*(cosTerm-slopeOfError[1])
@@ -44,15 +61,36 @@ public class Newton
     float secondDerivative(float[] acc, float alpha)
     {
         float sd=0;
+        for (int i = 0; i < 3; i++) {
+            acc[i]=acc[i]+calibration[i];
+        }
         float[] slopeOfError = errorData.getSlope("htc", alpha);
         float[] error = errorData.getError("htc", alpha);
-        float cosTerm= (float) (Math.cos(alpha)*GRAVITY_EARTH);
-        float sinTerm= (float) (Math.sin(alpha)*GRAVITY_EARTH);
+        float cosTerm= (float) (Math.cos(Math.toRadians(alpha))*GRAVITY_EARTH);
+        float sinTerm= (float) (Math.sin(Math.toRadians(alpha))*GRAVITY_EARTH);
         sd  = (float)   (2*Math.pow(slopeOfError[0],2)
                         +2*Math.pow((cosTerm-slopeOfError[1]),2) + 2*(acc[1]+sinTerm-error[1])*(-sinTerm)
                         +2*Math.pow((sinTerm-slopeOfError[2]),2) + 2*(acc[2]-cosTerm-error[2])*(cosTerm)
                         );
         return sd;
+    }
+    float[] getAcc(float[] acc, float alpha)
+    {
+        float[] a=new float[]{0,0,0};
+        float[] error = errorData.getError("htc", alpha);
+        a[0]=acc[0]-error[0];
+        a[1]= (float) (acc[1]- Math.sin(Math.toRadians(alpha))*GRAVITY_EARTH-error[1]);
+        a[2]= (float) (acc[2]- Math.cos(Math.toRadians(alpha))*GRAVITY_EARTH-error[2]);
+        return a;
+    }
+    float[] getAccWithoutError(float[] acc, float alpha)
+    {
+        float[] a=new float[]{0,0,0};
+        float[] error = errorData.getError("htc", alpha);
+        a[0]=acc[0];
+        a[1]= (float) (acc[1]- Math.sin(Math.toRadians(alpha))*GRAVITY_EARTH);
+        a[2]= (float) (acc[2]- Math.cos(Math.toRadians(alpha))*GRAVITY_EARTH);
+        return a;
     }
     float iterate(float alphaZero,float[] acc)
     {
